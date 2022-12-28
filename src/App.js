652aeffd714d8dc8fd1ff813f1ac453773b3bf64
aliabshv <eliabisov@gmail.com>
2022-12-28 13:02:20 +0400

    finis

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header';
 import Home from './pajes/home/Home';
 import MovieList from './components/movieList/movieList';
+import TopRated from './components/topRated/topRated';
+import UpComing from './components/upComing/upComing';
 function App() {
   return (
     <div className="App">
@@ -20,10 +22,11 @@ function App() {
           <Route path='/*' element={<h1>Error paje</h1>}> </Route> */}
 
           <Route index element={<Home />}></Route>
-          <Route path="movie/:id" element={<h1>Salam</h1>}></Route>
+          <Route path="movie/:id" element={<h1>Film Detail</h1>}></Route>
           <Route path="movie/:type" element={<h1>Salam</h1>}></Route>
           <Route path='movie/popular' element={<MovieList />}></Route>
-          <Route path='movie/top_rated' element={<MovieList />}></Route>
+          <Route path='movie/top_rated' element={<TopRated />}></Route>
+          <Route path='movie/upcoming' element={<UpComing/>}></Route>
           <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
       </Router>
---
+++ src/components/upComing/upComing.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useState} from "react"
+import "./upComing.css"
+import { useParams } from "react-router-dom"
+import Cards from "../card/Card"
+
+const UpComing = () => {
+    
+    const [movieList, setMovieList] = useState([])
+    const {type} = useParams()
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    useEffect(() => {
+        getData()
+    }, [type])
+
+    const getData = () => {
+        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "upcoming"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
+        .then(res => res.json())
+        .then(data => setMovieList(data.results))
+    }
+
+    return (
+        <div className="movie__list">
+            <h2 className="list__title">{(type ? type : "UpComing").toUpperCase()}</h2>
+            <div className="list__cards">
+                {
+                    movieList.map(movie => (
+                        <div key={movie?.id + movie?.overview}>
+                            <Cards movie={movie} />
+                        </div>
+                        
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default UpComing;
